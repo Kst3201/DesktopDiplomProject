f151fb21144b979dfd6e96d9387c5b40e60d8b30
Maksim <Kst3201@yandex.ru>
2026-03-04 20:20:31 +0300

    Добавьте файлы проекта.

---
+++ DesktopDiplomProject.Client/Views/Components/Pages/BlockPowersPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestDiplomProject.Views.Components.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для BlockPowersPage.xaml
+    /// </summary>
+    public partial class BlockPowersPage : Page
+    {
+        public BlockPowersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
